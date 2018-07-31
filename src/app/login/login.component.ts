5efe7d812b25eb15b6b93cf248310e7adc821ac6
mac-095-71 <mac-095-71@mac-095-71icxlocal.local>
2018-07-31 17:35:10 +0300

    added login form, started authentication part

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import {MatCardModule} from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormBuilder, FormControl, FormGroup, Validators  } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -12,9 +11,46 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  email = new FormControl('', [
+    Validators.required,
+    Validators.email
+  ]);
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.[A-Za-z])(?=.\d)[A-Za-z\d]{4,20}/)
+  ]);
+
+  constructor(private auth: AuthService,
+              private formBuilder: FormBuilder,
+              private router: Router) { }
+
 
   ngOnInit() {
+    if (this.auth.loggedIn) {
+      this.router.navigate(['/']);
+    }
+    this.loginForm = this.formBuilder.group({
+      email: this.email,
+      password: this.password
+    });
   }
+
+  getErrorEmail() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+      this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  getErrorPass() {
+    return this.password.hasError('required') ? 'You must enter a value' :
+      this.password.hasError('pattern') ? 'Invalid password' : '';
+  }
+
+  login() {
+    this.auth.login(this.loginForm.value).subscribe(
+      res => this.router.navigate(['/']),
+      error => false
+    );
+  }
+
 
 }
